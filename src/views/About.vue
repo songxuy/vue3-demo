4f5c6e7f407ea8d98fd87d7839b48117e2575f21
songxuyuan <541046145@qq.com>
2020-09-16 22:19:26 +0800

    [ADD] 1. add demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 @click="changeName">{{ state.name }}</h1>
+    <h4>{{ age }}</h4>
+    <a-button @click="changeAge">add</a-button>
+    <a-input v-model:value="state.name" placeholder="Basic usage" />
+    <a-input v-model:value="age" placeholder="Basic usage" />
   </div>
 </template>
 <script>
-export default {};
+import { reactive, ref, watch } from 'vue';
+import { Button, Input } from 'ant-design-vue';
+export default {
+  components: {
+    [Button.name]: Button,
+    [Input.name]: Input,
+  },
+  setup() {
+    const state = reactive({
+      name: '子君',
+    });
+    const age = ref(0);
+    const changeName = () => {
+      state.name = 'hello, world';
+    };
+    const changeAge = () => {
+      age.value++;
+    };
+    watch(age, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
+    });
+    return {
+      state,
+      changeName,
+      age,
+      changeAge,
+    };
+  },
+};
 </script>
