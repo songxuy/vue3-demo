1bbda7d8cf32ffffa262a8a3cadd613f3a936cb5
songxuyuan <541046145@qq.com>
2021-07-20 15:30:42 +0800

    [feat]1. scroll num

--- src/router/index.ts
+++ src/router/index.ts
@@ -87,6 +87,14 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/heart.vue'),
   },
+  {
+    path: '/numTest',
+    name: 'numTest',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/numTest.vue'),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/numTest.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="flex">
+    <ScrollNum v-for="(num, idx) of numArr" :key="idx" as="li" :i="num" :delay="idx + 1" />
+  </ul>
+</template>
+
+<script>
+import ScrollNum from '@/components/scrollNum.vue';
+
+export default {
+  name: 'App',
+  components: { ScrollNum },
+  data: () => ({ num: 886 }),
+  computed: {
+    numArr() {
+      const str = String(this.num);
+
+      return [parseInt(str[0]), parseInt(str[1]), parseInt(str[2])];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  position: relative;
+  width: 100px;
+  margin: 30px auto;
+  align-items: center;
+  justify-content: space-evenly;
+}
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+</style>
